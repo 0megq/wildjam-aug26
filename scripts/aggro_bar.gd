c2f286c49666d2a907be2e4c3371cd7d9f6cc995
ngui <nguyentlm787@gmail.com>
2026-08-23 04:05:05 -0700

    aggro bar added as seperate scene

---
+++ scripts/aggro_bar.gd
@@ -0,0 +1,37 @@
+extends TextureProgressBar
+
+@export var frame_normal: Texture2D
+@export var frame_charged: Texture2D
+@export var frame_hover: Texture2D
+
+@export var charge_threshold: float = 0.8
+
+var is_hovered := false
+
+func _ready() -> void:
+	mouse_filter = Control.MOUSE_FILTER_STOP
+	_update_frame()
+
+#TEST
+#func _process(delta: float) -> void:
+#	if Input.is_action_just_pressed("charging"):
+#		value += 5
+
+func _on_mouse_entered() -> void:
+	is_hovered = true
+	_update_frame()
+
+func _on_mouse_exited() -> void:
+	is_hovered = false
+	_update_frame()
+
+func _on_value_changed(_new_value: float) -> void:
+	_update_frame()
+
+func _update_frame() -> void:
+	if is_hovered:
+		texture_over = frame_hover
+	elif (value / max_value) >= charge_threshold:
+		texture_over = frame_charged
+	else:
+		texture_over = frame_normal
